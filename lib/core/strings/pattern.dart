ba364de4ac5800e93a3e19cdd9f08201da9d0051
hussienAlasol1 <alashol775076388hussien@gmail.com>
2025-07-06 05:00:37 +0300

    update i pattern string 393

--- lib/core/strings/pattern.dart
+++ lib/core/strings/pattern.dart
@@ -2,5 +2,6 @@ String emailPattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$';
 String x = "hello";
 String p = "hello hussien";
 String pp = "hello hussien";
+String ppppc = "hello hussien";
 
 String pppp = "hello hussien";
